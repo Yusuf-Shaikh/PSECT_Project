6c2868b68e577e7f3213bf3360b0d9b2900a03be
Yusuf-Shaikh <yusufshaikhrs26@gmail.com>
2022-11-28 12:47:03 +0530

    yeah

--- src/app/output/output.component.ts
+++ src/app/output/output.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { GeneratorsService } from '../generators.service';
+import { Component, OnInit } from "@angular/core";
+import { GeneratorsService } from "../generators.service";
 
 @Component({
-  selector: 'app-output',
-  templateUrl: './output.component.html',
-  styleUrls: ['./output.component.css'],
+  selector: "app-output",
+  templateUrl: "./output.component.html",
+  styleUrls: ["./output.component.css"],
 })
 export class OutputComponent implements OnInit {
+  constructor(private generatorsService: GeneratorsService) {}
+
   chartOptions: {
     title: { text: string };
     animationEnabled: boolean;
-    axisX:{title:string, interval:number};
-    axisY:{title:string, includeZero: boolean};
+    axisX: { title: string; interval: number };
+    axisY: { title: string; includeZero: boolean };
     data: {
       type: string;
       indexLabelFontColor: string;
@@ -21,28 +23,45 @@ export class OutputComponent implements OnInit {
     backgroundColor: string;
   };
   colors: string[] = [];
-  flapc: { value: number; index: number }[] = this.generatorsService.get_flapc();
- LegendText: string[] = [];
-
-  constructor(private generatorsService: GeneratorsService) {}
+  flapc: {
+    value: number;
+    index: number;
+  }[] = this.generatorsService.get_flapc();
+  LegendText: string[] = [];
+  generators: {
+    A_parameter: number;
+    B_parameter: number;
+    C_parameter: number;
+    Pmin: number;
+    Pmax: number;
+  }[] = this.generatorsService.get_generator();
+  power_list: number[] = [];
+  Output_list: {
+    generators: string;
+    Total_Pmax: number;
+    Total_Pmin: number;
+  }[] = [];
+  priority_list: {
+    value: number;
+    index: number;
+  }[][] = this.generatorsService.get_list();
+  dps: { x: number; y: number; color: string }[] = [];
 
   ngOnInit(): void {
-    console.log(this.flapc);
     for (let i = 0; i < this.priority_list.length; i++) {
       this.Output_list.push({
-        generators:"",
+        generators: "",
         Total_Pmax: 0,
-        Total_Pmin:0
-      })
+        Total_Pmin: 0,
+      });
       this.LegendText.push("");
       let Maxpower = 0;
       let Minpower = 0;
       for (let j = 0; j < this.priority_list[i].length; j++) {
         let generator_id = this.priority_list[i][j].index;
-        this.Output_list[i].generators +=
-          'G' + (generator_id + 1).toString();
-        this.LegendText[i] += "G"+(generator_id + 1).toString();
-        if(j!=this.priority_list[i].length-1){
+        this.Output_list[i].generators += "G" + (generator_id + 1).toString();
+        this.LegendText[i] += "G" + (generator_id + 1).toString();
+        if (j != this.priority_list[i].length - 1) {
           this.Output_list[i].generators += "+";
           this.LegendText[i] += "+";
         }
@@ -55,20 +74,26 @@ export class OutputComponent implements OnInit {
     }
 
     this.chartOptions = {
-      title: { text: '' },
+      title: { text: "" },
       animationEnabled: true,
-      axisX: {title:"Load Hour", interval:1 },
-      axisY: {title:"Power (MW)", includeZero: true },
+      axisX: { title: "Load Hour", interval: 1 },
+      axisY: { title: "Power (MW)", includeZero: true },
       data: this.createData(),
-      backgroundColor: '#f8f4e5',
+      backgroundColor: "#f8f4e5",
     };
 
-
-    let ggColors = ["#fea481","#7d8bff","#eaa6fd","#b9ffc3","#fc7474","#fcf581"];
+    let ggColors = [
+      "#fea481",
+      "#7d8bff",
+      "#eaa6fd",
+      "#b9ffc3",
+      "#fc7474",
+      "#fcf581",
+    ];
     for (let i = 0; i < this.power_list.length; i++) {
-      if(i<ggColors.length){
+      if (i < ggColors.length) {
         this.colors.push(ggColors[i]);
-      }else{
+      } else {
         this.colors.push(this.getRandomColor());
       }
     }
@@ -89,20 +114,6 @@ export class OutputComponent implements OnInit {
     }
   }
 
-  generators: {
-    A_parameter: number;
-    B_parameter: number;
-    C_parameter: number;
-    Pmin: number;
-    Pmax: number;
-  }[] = this.generatorsService.get_generator();
-
-  power_list: number[] = [];
-  Output_list:{generators:string; Total_Pmax:number; Total_Pmin:number}[] = [];
-  priority_list: { value: number; index: number }[][] =
-    this.generatorsService.get_list();
-  dps: { x: number; y: number; color: string }[] = [];
-
   createData() {
     let data: {
       type: string;
@@ -112,17 +123,17 @@ export class OutputComponent implements OnInit {
     }[] = [];
 
     data.push({
-      type: 'column',
-      indexLabelFontColor: '#5A5757',
+      type: "column",
+      indexLabelFontColor: "#5A5757",
       color: this.colors[0],
       dataPoints: this.createDataset(),
     });
 
     for (let i = 1; i < this.colors.length; i++) {
-      let temp: string = 'Slab' + (i + 1).toString();
+      let temp: string = "Slab" + (i + 1).toString();
       data.push({
-        type: 'column',
-        indexLabelFontColor: '#5A5757',
+        type: "column",
+        indexLabelFontColor: "#5A5757",
         color: this.colors[i],
         dataPoints: [],
       });
@@ -133,12 +144,12 @@ export class OutputComponent implements OnInit {
 
   createDataset() {
     let loads: number[] = this.generatorsService.get_load();
-    console.log(loads)
+    console.log(loads);
     for (let i = 0; i < loads.length; i++) {
       this.dps.push({
         x: i + 1,
         y: loads[i],
-        color: 'rgb(90, 220, 0)',
+        color: "rgb(90, 220, 0)",
       });
     }
     return this.dps;
@@ -146,6 +157,6 @@ export class OutputComponent implements OnInit {
 
   getRandomColor() {
     var color = Math.floor(0x1000000 * Math.random()).toString(16);
-    return '#' + ('000000' + color).slice(-6);
+    return "#" + ("000000" + color).slice(-6);
   }
 }
